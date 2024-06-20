294c9409eec5a21da2f7a3469b3138ddf4209113
HirisGu <hiris1228@gmail.com>
2024-06-20 11:34:57 +0100

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -4,7 +4,7 @@ import streamlit as st
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+    "[View the source code](https://github.com/hiris1228/pkb_chatbot/edit/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
 st.title("🌍🤖️💬 PKB Chatbot Demo")
