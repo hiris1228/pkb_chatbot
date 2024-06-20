5c2ec6397ccfa0b79e310f1822fd3fe1d7a54382
HirisGu <hiris1228@gmail.com>
2024-06-20 11:00:48 +0100

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -7,10 +7,20 @@ with st.sidebar:
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-st.title("💬 Chatbot")
-st.caption("🚀 A Streamlit chatbot powered by OpenAI")
+st.title("🌍🤖️💬 PKB Chatbot Demo")
+st.caption("🚀 A Very Friendly chatbot powered by OpenAI and AWS")
+
+"""
+This is a draft of PKB Chatbot Demo
+1⃣️📷Image OCR - where you can upload a label and extract text information.
+2⃣️🔍Chat with Search - where you can ask the PKB Chatbot to search more precise botanist content via the World Wide Web (Very difficult to rate its performance😂)
+3⃣️🌳Chat with PKB - where you can chat with the PKB Chatbot and allow interaction with our PKB Neptune Could service using Natural Language
+(Please remember that the 'Chat with PKB' is not currently fully functioning due to Neptune Engine Issue. I will fix it in a day or two.)
+4⃣️🗺️Graph Navigator - coming soon!
+"""
+
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state["messages"] = [{"role": "assistant", "content": "Hello, I'm the NHM PKB project assistant. How can I help you today?"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
