6098938b2a564c6a501abe2ca87d316d039574b1
HirisGu <hiris1228@gmail.com>
2024-06-20 01:42:59 +0100

    Update 1_OCR.py

--- pages/1_File_Q&A.py
+++ pages/1_File_Q&A.py
@@ -9,7 +9,7 @@ with st.sidebar:
     anthropic_api_key = st.text_input("Anthropic API Key", key="file_qa_api_key", type="password")
     #"[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/1_File_Q%26A.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
-
+'''
 st.title("📝 File Q&A with Anthropic")
 uploaded_file = st.file_uploader("Upload an article", type=("txt", "md"))
 question = st.text_input(
@@ -35,7 +35,7 @@ if uploaded_file and question and anthropic_api_key:
     )
     st.write("### Answer")
     st.write(response.completion)
-
+'''
 st.title("Specimen Label OCR with OpenAI GPT")
 
 # Upload image
