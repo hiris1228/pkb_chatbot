8c9e118d493658e87a78f8b09e816002c711b720
HirisGu <hiris1228@gmail.com>
2024-06-24 01:02:13 +0100

    Update and rename Chatbot.py to PKB_Chatbot.py

--- PKB_Chatbot.py
+++ PKB_Chatbot.py
@@ -17,14 +17,14 @@ This is a draft of PKB Chatbot Demo
 📷 1. Image OCR - where you can upload a label and extract text information.
 """
 """
-🔍 2. Chat with Search - where you can ask the PKB Chatbot to search more precise botanist content via the World Wide Web (Very difficult to rate its performance😂)
+🔍 2. Chat with Search - where you can ask the PKB Chatbot to search more precise botanist content via the World Wide Web. (Very difficult to rate its performance 😂)
 """
 """
 🌳 3. Chat with PKB - where you can chat with the PKB Chatbot and allow interaction with our PKB Neptune Could service using Natural Language
 (Please remember that the 'Chat with PKB' is not currently fully functioning due to Neptune Engine Issue. I will fix it in a day or two.)
 """
 """
-🗺️ 4. Graph Navigator - coming soon!
+🗺️ 4. Graph Navigator—Visualisation of a part of the PKB graph. (We are currently experiencing a connection loss due to the Neptune issue!)
 """
 """
 """
